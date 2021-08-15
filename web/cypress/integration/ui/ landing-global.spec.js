c4a0a6933ed3d7eb0c11a2c81f61634b0dcc10fd
gscode <adm.guilhermedesousa@gmail.com>
2021-08-15 15:17:36 -0300

    Cobertura de código usando os testes de UI

--- web/cypress/integration/ui/ landing-global.spec.js
+++ web/cypress/integration/ui/ landing-global.spec.js
@@ -1,13 +1,10 @@
 /// <reference types="cypress" />
 
-const LARGURAS = [1200, 1090] //1200 acima e 1090 abaixo do ponto de quebra da tela
+// Teste definido com largura global
 
-LARGURAS.forEach(largura => {
-  context(`Landing Page - ${largura}px`, () => { //HACK - Com o uso de templates literals o teste 
+  context('Landing Page', () => { 
     beforeEach(() => {
       cy.visit('/')
-      cy.viewport(largura, 900 ) //width = largura e Height = altura
-      cy.log('Largura: ', largura)
     })
 
     it('Navegar para o cadastro de aulas', () => {
@@ -18,7 +15,5 @@ LARGURAS.forEach(largura => {
     it('Navegar para pesquisa de professores', () => {
       cy.get('div a.study').click()
       cy.url().should('contain', 'study')
-      
     })
   })
-})
